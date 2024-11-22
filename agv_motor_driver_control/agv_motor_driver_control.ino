4d23825aa1da931d457eb401877db7e49261d8c1
biamterdex <mohamedkarimziadi@gmail.com>
2024-11-22 18:15:11 +0900

    values definitions and node initialization

--- agv_motor_driver_control/agv_motor_driver_control.ino
+++ agv_motor_driver_control/agv_motor_driver_control.ino
@@ -1,8 +1,22 @@
 #include <ModbusMaster.h>
 #include <SoftwareSerial.h>
 
+ModbusMaster node;
+
+#define RS485_RX 10 
+#define RS485_TX 11
+#define RS485_ENABLE_PIN 2
+
+SoftwareSerial RS485Serial(RS485_RX,RS485_TX);
+
+const uint16_t MOVE_DISTANCE = 600; // Unit Movement Distance in mm
+const uint16_t SPEED = 1000; // Command Rotation Speed in rpm
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  RS485Serial.begin(9600);
+  Serial.println("AGV Motor Control Initialized");
+  node.begin(1, RS485Serial);
 
 }
 
